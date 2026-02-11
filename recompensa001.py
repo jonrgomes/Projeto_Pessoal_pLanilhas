194469245e230180e2449026bd397adae1d0a176
Jonatas Ribeiro Gomes <jonrgomes@gmail.com>
2026-02-10 22:25:09 -0300

    DIA001

--- recompensa001.py
+++ recompensa001.py
@@ -1,9 +1,9 @@
-input=('Digite seu score:')
-if score ==5
+score=int(input('Digite seu score:'))
+if score ==5:
     print('Semana perfeita! Recompensa máxima liberada!')
-elif score >=3
+elif score >=3:
     print('Boa semana! Recompensa parcial liberada!')
-elif score >=1
+elif score >=1:
     print('Semana fraca! Sem recompensa!')
 else:
     print('Semana falha! Reset necessário!!!')
