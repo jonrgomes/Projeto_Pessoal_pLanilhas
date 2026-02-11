aea5c66c9268253323682adfc9d7175343268f5d
jonrgomes <jonrgomes@gmail.com>
2026-02-10 22:00:52 -0300

    Dia001

---
+++ recompensa001.py
@@ -0,0 +1,10 @@
+score=('Digite seu score:')
+if score ==5
+    print('Semana perfeita! Recompensa máxima liberada!')
+elif score >=3
+    print('Boa semana! Recompensa parcial liberada!')
+elif score >=1
+    print('Semana fraca! Sem recompensa!')
+else:
+    print('Semana falha! Reset necessário!!!')
+    
